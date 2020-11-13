e9708440b208bb4f529490b0a3e59afe2c30be1e
Brian Dwyer <Brian.Dwyer@broadridge.com>
2020-11-13 13:55:55 -0500

    Disable insecure ciphers
    Signed-off-by: Brian Dwyer <Brian.Dwyer@broadridge.com>

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -12,6 +12,12 @@ fi
 cat << EOF > /etc/stunnel/redislabs.conf
 cafile = /etc/ssl/certs/ca-certificates.crt
 verify = 2
+options = NO_SSLv2
+options = NO_SSLv3
+options = NO_TLSv1
+options = NO_TLSv1.1
+options = CIPHER_SERVER_PREFERENCE
+options = DONT_INSERT_EMPTY_FRAGMENTS
 socket = l:TCP_NODELAY=1
 socket = r:TCP_NODELAY=1
 delay = yes
